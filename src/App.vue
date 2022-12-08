9d4b90c16058d010646e9de507541017f9574b76
twilight4j <twilight4j@gmail.com>
2022-12-08 22:21:04 +0900

    04 Header - nav

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Header />
+  <RouterView />
+</template>
+
+<script>
+import Header from '~/components/Header'
+
+export default {
+  components : {
+    Header
+  },
+  data() {
+    return {
+      message: 'Hello Vue!!!'
+    }
+  }
+}
+</script>
+<style lang="scss">
+@import "~scss/main";
+</style>
