72f875779965f02712f64892187f4d6dfcecc145
twilight4j <twilight4j@gmail.com>
2023-01-26 23:15:49 +0900

    Ch04 영화검색사이트예제 18 영화아이템-기본출력

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="class">
+  <div class="container">
     <div class="inner">
+      <div class="message">
+        {{ message }}
+      </div>
       <!-- MovieItem 에 props로 전달 -->
-      <MovieItem
-        v-for="movie in movies"
-        :key="movie.imdbID"
-        :movie="movie" /> 
+      <div class="movies">
+        <MovieItem
+          v-for="movie in movies"
+          :key="movie.imdbID"
+          :movie="movie" />
+      </div> 
     </div>
   </div>
 </template>
@@ -21,7 +26,20 @@ export default {
     // 최초 비어있는 데이터가 아니라 api를 통해 받아온 movies를 사용하기 위해
     movies() {
       return this.$store.state.movie.movies
+    },
+    message() {
+      return this.$store.state.movie.message
     }
   } 
 }
 </script>
+
+<style lang="scss" scoped>
+.container {
+  .movies {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+}
+</style>
--- src/store/movie.js
+++ src/store/movie.js
@@ -26,32 +26,63 @@ export default {
   },
   actions: {
     async searchMovies({ state, commit }, payload) {
-      const { title, type, number, year } = payload
-      const OMDB_API_KEY = '7035c60c'
-      const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
-      const { Search, totalResults } = res.data
-      commit('updateState', {
-        movies: _uniqBy(Search, 'imdbID')
-      })
+      try {
+        const res = await _fetchMovie({
+          ...payload,
+          page : 1
+        })
+        const { Search, totalResults } = res.data
+        commit('updateState', {
+          movies: _uniqBy(Search, 'imdbID')
+        })
 
-      console.log(totalResults) // 268 => 27
-      console.log(typeof totalResults) // string
-      const total = parseInt(totalResults, 10) //10진법
-      const pageLength = Math.ceil(total / 10)
-      
-      // 추가 요청!
-      if(pageLength > 1) {
-        for(let page = 2; page <= pageLength; page +=1) {
-          if(page > (number / 10)) break
-          const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
-          const { Search } = res.data
-          commit('updateState', {
-            movies: [
-              ...state.movies, 
-              ..._uniqBy(Search, 'imdbID')] // 기존 movies 부터 전개하여 할당하고 검색한 데이터를 전개하여 할당
-          })
+        console.log(totalResults) // 268 => 27
+        console.log(typeof totalResults) // string
+        const total = parseInt(totalResults, 10) //10진법
+        const pageLength = Math.ceil(total / 10)
+        
+        // 추가 요청!
+        if(pageLength > 1) {
+          for(let page = 2; page <= pageLength; page +=1) {
+            if(page > (payload.number / 10)) break
+            const res = await _fetchMovie({
+              ...payload,
+              page
+            })
+            const { Search } = res.data
+            commit('updateState', {
+              movies: [
+                ...state.movies, 
+                ..._uniqBy(Search, 'imdbID')] // 기존 movies 부터 전개하여 할당하고 검색한 데이터를 전개하여 할당
+            })
+          }
         }
+      } catch (message) {
+        commit('updateState',{
+          movies: [],
+          message
+        })
       }
     }
   }
+}
+
+function _fetchMovie(payload) {
+  const { title, type, year, page } = payload
+  const OMDB_API_KEY = '7035c60c'
+  const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+
+  return new Promise((resolve, reject) => {
+    axios.get(url)
+    .then(res => {
+      if(res.data.Error){
+        reject(res.data.Error)
+        return
+      }
+      resolve(res)
+    })
+    .catch(err => {
+      reject(err.message)
+    })
+  })
 }
